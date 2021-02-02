f877e463c9a50ff6aa24809d995a8f87acd6171d
Aaron Gertler <aaron.gertler@gmail.com>
2021-02-02 14:19:28 -0600

    testing initial layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import React, { useState } from 'react'
-import { render } from 'react-dom'
+import React from 'react'
 
-import { DatePicker, message } from 'antd'
+import { Layout, Menu } from 'antd'
+import {
+  AppstoreOutlined,
+  BarChartOutlined,
+  CloudOutlined,
+  ShopOutlined,
+  TeamOutlined,
+  UserOutlined,
+  UploadOutlined,
+  VideoCameraOutlined
+} from '@ant-design/icons'
 
 import './App.css';
 
+const { Header, Content, Footer, Sider } = Layout
+
 function App() {
-  const [date, setDate] = useState(null)
-
-  const handleChange = value => {
-    message.info(`Selected Date: ${value ? value.format('YYYY-MM-DD') : 'None'}`)
-    setDate(value)
-  }
-
   return (
-    <div className="App" style={{ width: 400, margin: '100px auto' }}>
-      <DatePicker onChange={handleChange} />
+    <div className="App">
+      <Layout>
+        <Sider style={{ overflow: 'auto', height: '100vh', position: 'fixed', left: 0 }}>
+          <div className='logo' />
 
-      <div style={{ marginTop: 16 }}>
-        Selected Date: {date ? date.format('YYYY-MM-DD') : 'None'}
-      </div>
+          <Menu theme='dark' mode='inline' defaultSelectedKEys={['4']}>
+            <Menu.Item key='1' icon={<UserOutlined />}>
+              nav 1
+            </Menu.Item>
+
+            <Menu.Item key='2' icon={<VideoCameraOutlined />}>
+              nav 2
+            </Menu.Item>
+
+            <Menu.Item key='3' icon={<UploadOutlined />}>
+              nav 3
+            </Menu.Item>
+
+            <Menu.Item key='4' icon={<BarChartOutlined />}>
+              nav 4
+            </Menu.Item>
+
+            <Menu.Item key='5' icon={<CloudOutlined />}>
+              nav 5
+            </Menu.Item>
+
+            <Menu.Item key='6' icon={<AppstoreOutlined />}>
+              nav 6
+            </Menu.Item>
+
+            <Menu.Item key='7' icon={<TeamOutlined />}>
+              nav 7
+            </Menu.Item>
+
+            <Menu.Item key='8' icon={<ShopOutlined />}>
+              nav 8
+            </Menu.Item>
+          </Menu>
+        </Sider>
+      </Layout>
     </div>
   );
 }
