52e041735c6c25d9e73b666a709a402744133a35
Aaron Gertler <aaron.gertler@gmail.com>
2021-02-03 15:06:31 -0600

    created auth, Login.js, and auth.sass

---
+++ src/components/auth/Login.js
@@ -0,0 +1,13 @@
+import React, { useRef } from 'react'
+import { useHistory } from 'react-router-dom'
+import { Row, Col } from 'antd'
+
+import './auth.sass'
+
+export const Login = (props) => {
+    return (
+        <Row justify='center' align='middle' id='loginContainer'>
+            Hello world!
+        </Row>
+    )
+}
