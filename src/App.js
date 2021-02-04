5a27b6c36608d9fc3230d0f5a13d6f296ec3656c
Aaron Gertler <aaron.gertler@gmail.com>
2021-02-04 13:55:39 -0600

    moved Sidebar content into Sidebar.js

--- src/App.js
+++ src/App.js
@@ -2,18 +2,9 @@ import React from 'react'
 
 import { Login } from './components/auth/Login'
 import { Register } from './components/auth/Register'
+import { Sidebar } from './components/Sidebar/Sidebar'
 
-import { Layout, Menu } from 'antd'
-import {
-  AppstoreOutlined,
-  BarChartOutlined,
-  CloudOutlined,
-  ShopOutlined,
-  TeamOutlined,
-  UserOutlined,
-  UploadOutlined,
-  VideoCameraOutlined
-} from '@ant-design/icons'
+import { Layout } from 'antd'
 
 const { Header, Content, Footer, Sider } = Layout
 
@@ -23,43 +14,7 @@ function App() {
 
   return (
     <Layout>
-      <Sider id='sidebarMain' style={{ overflow: 'auto', height: '100vh', position: 'sticky', top: 0, left: 0 }} collapsible>
-        <div id='logo' />
-
-        <Menu theme='dark' mode='inline' defaultSelectedKeys={['1']} id='sidebarMainUl'>
-          <Menu.Item key='1' icon={<UserOutlined />}>
-            nav 1
-            </Menu.Item>
-
-          <Menu.Item key='2' icon={<VideoCameraOutlined />}>
-            nav 2
-            </Menu.Item>
-
-          <Menu.Item key='3' icon={<UploadOutlined />}>
-            nav 3
-            </Menu.Item>
-
-          <Menu.Item key='4' icon={<BarChartOutlined />}>
-            nav 4
-            </Menu.Item>
-
-          <Menu.Item key='5' icon={<CloudOutlined />}>
-            nav 5
-            </Menu.Item>
-
-          <Menu.Item key='6' icon={<AppstoreOutlined />}>
-            nav 6
-            </Menu.Item>
-
-          <Menu.Item key='7' icon={<TeamOutlined />}>
-            nav 7
-            </Menu.Item>
-
-          <Menu.Item key='8' icon={<ShopOutlined />}>
-            nav 8
-            </Menu.Item>
-        </Menu>
-      </Sider>
+      <Sidebar />
 
       <Layout className='site-layout'>
         <Header className='site-layout-background' style={{ padding: 0 }} />
