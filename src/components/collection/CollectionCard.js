a8eab6fd3644357556b7365c1d75e2193b8c05be
Aaron Gertler <aaron.gertler@gmail.com>
2021-02-04 15:32:31 -0600

    created CollectionCard for sidebar

---
+++ src/components/collection/CollectionCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import { Menu } from 'antd'
+import { AppstoreOutlined } from '@ant-design/icons'
+
+export const CollectionCard = ({ collection }) => {
+    <Menu.Item key='1' icon={<AppstoreOutlined />} id={collection.id}>
+        <Link to={`/collection/${collection.id}`}>
+            {collection.name}
+        </Link>
+    </Menu.Item>
+}
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,16 +1,7 @@
 import React from 'react'
 
 import { Layout, Menu } from 'antd'
-import {
-    AppstoreOutlined,
-    BarChartOutlined,
-    CloudOutlined,
-    ShopOutlined,
-    TeamOutlined,
-    UserOutlined,
-    UploadOutlined,
-    VideoCameraOutlined
-} from '@ant-design/icons'
+import { AppstoreOutlined } from '@ant-design/icons'
 
 const { Sider } = Layout
 
@@ -19,38 +10,10 @@ export const Sidebar = () => {
         <Sider id='sidebarMain' style={{ overflow: 'auto', height: '100vh', position: 'sticky', top: 0, left: 0 }} collapsible >
             <div id='logo' />
 
-            <Menu theme='dark' mode='inline' defaultSelectedKeys={['1']} id='sidebarMainUl'>
-                <Menu.Item key='1' icon={<UserOutlined />}>
-                    nav 1
-            </Menu.Item>
-
-                <Menu.Item key='2' icon={<VideoCameraOutlined />}>
-                    nav 2
-            </Menu.Item>
-
-                <Menu.Item key='3' icon={<UploadOutlined />}>
-                    nav 3
-            </Menu.Item>
-
-                <Menu.Item key='4' icon={<BarChartOutlined />}>
-                    nav 4
-            </Menu.Item>
-
-                <Menu.Item key='5' icon={<CloudOutlined />}>
-                    nav 5
-            </Menu.Item>
-
-                <Menu.Item key='6' icon={<AppstoreOutlined />}>
-                    nav 6
-            </Menu.Item>
-
-                <Menu.Item key='7' icon={<TeamOutlined />}>
-                    nav 7
-            </Menu.Item>
-
-                <Menu.Item key='8' icon={<ShopOutlined />}>
-                    nav 8
-            </Menu.Item>
+            <Menu theme='dark' mode='inline' id='sidebarMainUl'>
+                <Menu.Item key='1' icon={<AppstoreOutlined />}>
+                    First Stockpile
+                </Menu.Item>
             </Menu>
         </Sider>
     )
