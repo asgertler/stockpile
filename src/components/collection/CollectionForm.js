f3dc03bcf05af554a999d5b2448b65329991c61c
Aaron Gertler <aaron.gertler@gmail.com>
2021-02-04 18:56:46 -0600

    created Collection

---
+++ src/components/collection/CollectionForm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { useHistory, useParams } from 'react-router-dom'
+import { CollectionContext } from './CollectionProvider'
+
+import { Row, Col, Form, Input, Button, Checkbox, message, Modal } from 'antd'
+
+export const CollectionForm = () => {
+
+}
