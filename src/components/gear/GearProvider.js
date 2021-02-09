28ccb84c0ef62719756ee27ebb37cf5ca1dc00b6
Aaron Gertler <aaron.gertler@gmail.com>
2021-02-08 19:47:19 -0600

    getGear created

--- src/components/gear/GearProvider.js
+++ src/components/gear/GearProvider.js
@@ -3,5 +3,11 @@ import React, { useState, createContext } from 'react'
 export const GearContext = createContext()
 
 export const GearProvider = (props) => {
+    const [gear, setGear] = useState([])
 
+    const getGear = () => {
+        return fetch('http://localhost:8088/gear?_expand=collection&_expand=user')
+            .then(res => res.json())
+            .then(setGear)
+    }
 }
