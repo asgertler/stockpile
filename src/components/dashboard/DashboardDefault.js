609a89570f1d758ebedac18c74fb03e9227d276f
Aaron Gertler <aaron.gertler@gmail.com>
2021-02-11 10:31:54 -0600

    added time-dependent greetings

--- src/components/dashboard/DashboardDefault.js
+++ src/components/dashboard/DashboardDefault.js
@@ -17,10 +17,22 @@ export const DashboardDefault = () => {
     const nameArr = fullName.split(" ")
     const firstName = nameArr[0]
 
+    const today = new Date()
+    const currentTime = today.getHours()
+
+    let timeGreeting
+    if (4 <= currentTime <= 11) {
+        timeGreeting = "Good Morning, "
+    } else if (12 <= currentTime <= 7) {
+        timeGreeting = "Good Afternoon, "
+    } else {
+        timeGreeting = "Good Evening, "
+    }
+
     return (
         <Row justify='center' align='middle' className='default-row'>
             <Col className='default-col'>
-                <h2>Welcome to Stockpile, {firstName}!</h2>
+                <h2>{timeGreeting} {firstName}!</h2>
 
                 <p>Click on one of your collections in the sidebar to see your list of gear, or click the plus to add a new one!</p>
             </Col>
