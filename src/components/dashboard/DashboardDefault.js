7d3d54038be0d8394c8f71c009fa34ef03ced4fd
Aaron Gertler <aaron.gertler@gmail.com>
2021-02-11 13:14:11 -0600

    fixed time check

--- src/components/dashboard/DashboardDefault.js
+++ src/components/dashboard/DashboardDefault.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { UserContext } from '../user/UserProvider'
 
 import { Row, Col } from 'antd'
@@ -7,23 +7,24 @@ export const DashboardDefault = () => {
     const currentUser = parseInt(localStorage.getItem('stockpileUser'))
 
     const { users, getUsers } = useContext(UserContext)
+    const [user, setUser] = useState([])
 
     useEffect(() => {
         getUsers()
+            .then(console.log(users))
+        // .then(setUser(users.filter(user => user.id === currentUser)))
     }, [])
-
-    const userRef = users.filter(user => user.id === currentUser)
-    const fullName = userRef[0].name
-    const nameArr = fullName.split(" ")
-    const firstName = nameArr[0]
+    // const fullName = user.name
+    // const nameArr = fullName.split(" ")
+    // const firstName = nameArr[0]
 
     const today = new Date()
     const currentTime = today.getHours()
 
     let timeGreeting
-    if (4 <= currentTime <= 11) {
+    if (currentTime >= 4 && currentTime <= 11) {
         timeGreeting = "Good Morning, "
-    } else if (12 <= currentTime <= 7) {
+    } else if (currentTime >= 12 && currentTime <= 19) {
         timeGreeting = "Good Afternoon, "
     } else {
         timeGreeting = "Good Evening, "
@@ -32,7 +33,7 @@ export const DashboardDefault = () => {
     return (
         <Row justify='center' align='middle' className='default-row'>
             <Col className='default-col'>
-                <h2>{timeGreeting} {firstName}!</h2>
+                <h2>{timeGreeting} {'firstName'}!</h2>
 
                 <p>Click on one of your collections in the sidebar to see your list of gear, or click the plus to add a new one!</p>
             </Col>
